9c1a2f34171a284b2c26c28a8d0a927979df1f8d
Resul TÜZEN <resultuzen@gmail.com>
2023-10-23 10:08:47 +0300

    Update wowGame.py
    Thanks to @batuhanergun

--- wowGame.py
+++ wowGame.py
@@ -1,31 +1,36 @@
-import os, sys, random
-os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
-import pygame
-import RPi.GPIO as GPIO
 from time import sleep
+import RPi.GPIO as GPIO
+import pygame
+import os
+import sys
+import random
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+
+player1speed = 5
+player2speed = 5
 
 def ballAnimation():
-    global ballspeedx, ballspeedy, opponentspeed, pscore, oscore, hit, bounce
+    global ballspeedx, ballspeedy, player2speed, p1score, p2score, hit, bounce
     ball.x += ballspeedx
     ball.y += ballspeedy
-    if ball.top <= 0 or ball.bottom >= height: 
+    if ball.top <= 0 or ball.bottom >= height:
         ballspeedy *= -1
         bounce.play()
-    if ball.centerx <= 15 or ball.centerx >= width - 15: 
-        if ball.centerx < width/2 :
-            pscore += 1
+    if ball.centerx <= 15 or ball.centerx >= width - 15:
+        if ball.centerx < width/2:
+            p1score += 1
         else:
-            oscore += 1
+            p2score += 1
         goal.play()
         ballRestart()
         pygame.time.delay(1000)
-    if ball.colliderect(player): 
+    if ball.colliderect(player1):
         ballspeedx *= -1
         hit.play()
-    if ball.colliderect(opponent):
+    if ball.colliderect(player2):
         ballspeedx *= -1
-        opponentspeed = random.choice((3, 7))
         hit.play()
+
 
 def ballRestart():
     global ballspeedx, ballspeedy, start
@@ -34,31 +39,37 @@ def ballRestart():
     ballspeedx = 7 * random.choice((1, -1))
     ballspeedy = 7 * random.choice((1, -1))
 
-def playerAnimation(enkoder_value):
-    player.y += enkoder_value * playerspeed
-    if player.top <= 0: player.top = 0
-    if player.bottom >= height: player.bottom = height
 
-def opponentAI(enkoder_value):
-    if opponent.bottom < ball.centery : opponent.centery += enkoder_value * opponentspeed
-    if opponent.top > ball.centery : opponent.centery -= enkoder_value * opponentspeed
-    if opponent.top <= 0 : opponent.top = 0
-    if opponent.bottom >= height : opponent.bottom = height
+def player1Animation(enkoder_value):
+    player1.y += enkoder_value * player1speed
+    if player1.top <= 0:
+        player1.top = 0
+    if player1.bottom >= height:
+        player1.bottom = height
+
+
+def player2Animation(enkoder_value):
+    player2.y += enkoder_value * player2speed
+    if player2.top <= 0:
+        player2.top = 0
+    if player2.bottom >= height:
+        player2.bottom = height
+
 
 def printScore(surface):
-    global pscore, oscore
+    global p1score, p2score
     font = pygame.font.Font(None, 72)
-    text = font.render(str(oscore), True, gamecolor)
+    text = font.render(str(p2score), True, gamecolor)
     textRect = text.get_rect()
     textRect.center = (width/2-30, 42)
     surface.blit(text, textRect)
-    text = font.render(str(pscore), True, gamecolor)
+    text = font.render(str(p1score), True, gamecolor)
     textRect = text.get_rect()
     textRect.center = (width/2+30, 42)
     surface.blit(text, textRect)
 
-# GPIO pinlerini ayarla
 
+# GPIO pinlerini ayarla
 ENKODER1_DT = 19
 ENKODER1_CLK = 13
 ENKODER2_DT = 6
@@ -81,12 +92,9 @@ goal = pygame.mixer.Sound('goal.ogg')
 start = pygame.mixer.Sound('start.ogg')
 
 # Ekranı ayarla
-screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-
-width, height = screen.get_size()
-
 pygame.display.set_caption("Test")
-
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+width, height = screen.get_size()
 bgcolor = pygame.Color('grey12')
 gamecolor = pygame.Color('white')
 
@@ -95,18 +103,19 @@ ballcolor = pygame.Color('white')
 ballspeedx = ballspeedy = 0
 ballRestart()
 
-player = pygame.Rect(width-30, height/2-70, 20, 140)
-playerspeed = 0
+player1 = pygame.Rect(width-30, height/2-70, 20, 140)
+player2 = pygame.Rect(width-30, height/2-70, 20, 140)
 
-opponent = pygame.Rect(10, height/2-70, 20, 140)
-opponentspeed = 5
-
-pscore = 0
-oscore = 0
+p1score = 0
+p2score = 0
 
 # Enkoderlerin değerlerini tutmak için değişkenler
 enkoder1_value = 0
 enkoder2_value = 0
+
+# Initialize last states for both encoders
+enkoder1_clkLastState = GPIO.input(ENKODER1_CLK)
+enkoder2_clkLastState = GPIO.input(ENKODER2_CLK)
 
 while True:
     for event in pygame.event.get():
@@ -114,36 +123,38 @@ while True:
             pygame.quit()
             sys.exit()
 
-    # Her iki enkoderin değerlerini oku
+    # Read current states
     enkoder1_clk = GPIO.input(ENKODER1_CLK)
     enkoder1_dt = GPIO.input(ENKODER1_DT)
     enkoder2_clk = GPIO.input(ENKODER2_CLK)
     enkoder2_dt = GPIO.input(ENKODER2_DT)
 
     # Her iki enkoder için dönüş değerlerini hesapla
-    if enkoder1_clk != enkoder1_dt:
-        if enkoder1_clk:
+    if enkoder1_clk != enkoder1_clkLastState:
+        if enkoder1_dt != enkoder1_clk:
             enkoder1_value += 1
         else:
             enkoder1_value -= 1
+    enkoder1_clkLastState = enkoder1_clk
 
-    if enkoder2_clk != enkoder2_dt:
-        if enkoder2_clk:
+    if enkoder2_clk != enkoder2_clkLastState:
+        if enkoder2_dt != enkoder2_clk:
             enkoder2_value += 1
         else:
             enkoder2_value -= 1
+    enkoder2_clkLastState = enkoder2_clk
 
     # Oyun mantığını işle
     ballAnimation()
-    playerAnimation(enkoder1_value)
-    opponentAI(enkoder2_value)
+    player1Animation(enkoder1_value)
+    player2Animation(enkoder2_value)
 
     # Ekranı temizle ve çizimleri yap
     screen.fill(bgcolor)
     printScore(screen)
     pygame.draw.aaline(screen, gamecolor, (width/2, 0), (width/2, height))
-    pygame.draw.rect(screen, gamecolor, player)
-    pygame.draw.rect(screen, gamecolor, opponent)
+    pygame.draw.rect(screen, gamecolor, player1)
+    pygame.draw.rect(screen, gamecolor, player2)
     pygame.draw.ellipse(screen, ballcolor, ball)
 
     pygame.display.flip()
