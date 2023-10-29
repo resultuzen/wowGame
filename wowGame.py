09800188e44757be296ea47eda195d6059f1ded7
Resul TÜZEN <resultuzen@gmail.com>
2023-10-29 03:27:32 +0300

    Update wowGame.py

--- wowGame.py
+++ wowGame.py
@@ -119,7 +119,7 @@ enkoder2_clkLastState = GPIO.input(ENKODER2_CLK)
 
 while True:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             pygame.quit()
             sys.exit()
 
