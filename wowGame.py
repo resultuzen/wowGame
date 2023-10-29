4400e2e5f22d5f727da3c14827570a5cbf1f0aeb
Resul TÜZEN <resultuzen@gmail.com>
2023-10-29 04:04:14 +0300

    Update wowGame.py

--- wowGame.py
+++ wowGame.py
@@ -6,6 +6,32 @@ import sys
 import random
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 
+player1speed = 2
+player2speed = 2
+
+def ballAnimation():
+    global ballspeedx, ballspeedy, player2speed, p1score, p2score, hit, bounce
+    ball.x += ballspeedx
+    ball.y += ballspeedy
+    if ball.top <= 0 or ball.bottom >= height:
+        ballspeedy *= -1
+        bounce.play()
+    if ball.centerx <= 15 or ball.centerx >= width - 15:
+        if ball.centerx < width/2:
+            p1score += 1
+        else:
+            p2score += 1
+        goal.play()
+        ballRestart()
+        pygame.time.delay(1000)
+    if ball.colliderect(player1):
+        ballspeedx *= -1
+        hit.play()
+    if ball.colliderect(player2):
+        ballspeedx *= -1
+        hit.play()
+
+
 def ballRestart():
     global ballspeedx, ballspeedy, start
     ball.center = (width/2, height/2)
@@ -13,8 +39,44 @@ def ballRestart():
     ballspeedx = 7 * random.choice((1, -1))
     ballspeedy = 7 * random.choice((1, -1))
 
-player1speed = 2
-player2speed = 2
+def player1Animation(enkoder1_value):
+    if enkoder1_value > 0:
+        player1.y += player1speed
+        enkoder1_value -= 1
+    elif enkoder1_value < 0:
+        player1.y -= player1speed
+        enkoder1_value += 1
+
+    if player1.top <= 0:
+        player1.top = 0
+    if player1.bottom >= height:
+        player1.bottom = height
+
+def player2Animation(enkoder2_value):
+    if enkoder2_value > 0:
+        player2.y += player2speed
+        enkoder2_value -= 1
+    elif enkoder2_value < 0:
+        player2.y -= player2speed
+        enkoder2_value += 1
+
+    if player2.top <= 0:
+        player2.top = 0
+    if player2.bottom >= height:
+        player2.bottom = height
+            
+def printScore(surface):
+    global p1score, p2score
+    font = pygame.font.Font(None, 72)
+    text = font.render(str(p2score), True, gamecolor)
+    textRect = text.get_rect()
+    textRect.center = (width/2-30, 42)
+    surface.blit(text, textRect)
+    text = font.render(str(p1score), True, gamecolor)
+    textRect = text.get_rect()
+    textRect.center = (width/2+30, 42)
+    surface.blit(text, textRect)
+
 
 # GPIO pinlerini ayarla
 ENKODER1_DT = 19
@@ -50,11 +112,11 @@ ballcolor = pygame.Color('white')
 ballspeedx = ballspeedy = 0
 ballRestart()
 
-p1score = 0
-p2score = 0
-
 player1 = pygame.Rect(width - 30, height // 2 - 70, 20, 140)
 player2 = pygame.Rect(10, height // 2 - 70, 20, 140)
+
+p1score = 0
+p2score = 0
 
 # Enkoderlerin değerlerini tutmak için değişkenler
 enkoder1_value = 0
@@ -63,69 +125,6 @@ enkoder2_value = 0
 # Initialize last states for both encoders
 enkoder1_clkLastState = GPIO.input(ENKODER1_CLK)
 enkoder2_clkLastState = GPIO.input(ENKODER2_CLK)
-
-def ballAnimation():
-    global ballspeedx, ballspeedy, player2speed, p1score, p2score, hit, bounce
-    ball.x += ballspeedx
-    ball.y += ballspeedy
-    if ball.top <= 0 or ball.bottom >= height:
-        ballspeedy *= -1
-        bounce.play()
-    if ball.centerx <= 15 or ball.centerx >= width - 15:
-        if ball.centerx < width/2:
-            p1score += 1
-        else:
-            p2score += 1
-        goal.play()
-        ballRestart()
-        pygame.time.delay(1000)
-    if ball.colliderect(player1):
-        ballspeedx *= -1
-        hit.play()
-    if ball.colliderect(player2):
-        ballspeedx *= -1
-        hit.play()
-
-player1_target = player1.y
-player2_target = player2.y
-
-def player1Animation(enkoder1_value):
-    global player1_target
-    player1_target += enkoder1_value
-    if player1_target < 0:
-        player1_target = 0
-    if player1_target > height - player1.height:
-        player1_target = height - player1.height
-
-    if player1.y < player1_target:
-        player1.y += player1speed
-    elif player1.y > player1_target:
-        player1.y -= player1speed
-
-def player2Animation(enkoder2_value):
-    global player2_target
-    player2_target += enkoder2_value
-    if player2_target < 0:
-        player2_target = 0
-    if player2_target > height - player2.height:
-        player2_target = height - player2.height
-
-    if player2.y < player2_target:
-        player2.y += player2speed
-    elif player2.y > player2_target:
-        player2.y -= player2speed
-        
-def printScore(surface):
-    global p1score, p2score
-    font = pygame.font.Font(None, 72)
-    text = font.render(str(p2score), True, gamecolor)
-    textRect = text.get_rect()
-    textRect.center = (width/2-30, 42)
-    surface.blit(text, textRect)
-    text = font.render(str(p1score), True, gamecolor)
-    textRect = text.get_rect()
-    textRect.center = (width/2+30, 42)
-    surface.blit(text, textRect)
 
 while True:
     for event in pygame.event.get():
