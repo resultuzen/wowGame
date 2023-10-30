d4faca7866057d85cb133de96b22e8d15e0af2bf
Resul TÜZEN <resultuzen@gmail.com>
2023-10-30 23:59:26 +0300

    Batuhan'ın denediği kod 1

--- wowGame.py
+++ wowGame.py
@@ -6,18 +6,28 @@ import sys
 import random
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 
-player1speed = 2
-player2speed = 2
+
+class Player:
+    def __init__(self, width, height, speed):
+        self.width = width
+        self.height = height
+        self.speed = speed
+
+
+# Width - Height - Speed
+Player1 = Player(20, 140, 20)
+Player2 = Player(20, 140, 20)
+
 
 def ballAnimation():
-    global ballspeedx, ballspeedy, player2speed, p1score, p2score, hit, bounce
+    global ballspeedx, ballspeedy, p1score, p2score, hit, bounce
     ball.x += ballspeedx
     ball.y += ballspeedy
-    if ball.top <= 0 or ball.bottom >= height:
+    if ball.top <= 0 or ball.bottom >= screenHeight:
         ballspeedy *= -1
         bounce.play()
-    if ball.centerx <= 15 or ball.centerx >= width - 15:
-        if ball.centerx < width/2:
+    if ball.centerx <= 15 or ball.centerx >= screenWidth - 15:
+        if ball.centerx < screenWidth / 2:
             p1score += 1
         else:
             p2score += 1
@@ -34,47 +44,34 @@ def ballAnimation():
 
 def ballRestart():
     global ballspeedx, ballspeedy, start
-    ball.center = (width/2, height/2)
+    ball.center = (screenWidth / 2, screenHeight / 2)
     start.play()
     ballspeedx = 7 * random.choice((1, -1))
     ballspeedy = 7 * random.choice((1, -1))
 
-def player1Animation(enkoder1_value):
-    if enkoder1_value > 0:
-        player1.y += player1speed
-        enkoder1_value -= 1
-    elif enkoder1_value < 0:
-        player1.y -= player1speed
-        enkoder1_value += 1
 
-    if player1.top <= 0:
-        player1.top = 0
-    if player1.bottom >= height:
-        player1.bottom = height
+def player1Animation(encoder_value):
+    player1.y = encoder_value * Player1.speed
+    if player1.top <= 0: player1.top = 0
+    if player1.bottom >= player1: player1.bottom = screenHeight
 
-def player2Animation(enkoder2_value):
-    if enkoder2_value > 0:
-        player2.y += player2speed
-        enkoder2_value -= 1
-    elif enkoder2_value < 0:
-        player2.y -= player2speed
-        enkoder2_value += 1
 
-    if player2.top <= 0:
-        player2.top = 0
-    if player2.bottom >= height:
-        player2.bottom = height
-            
+def player2Animation(encoder_value):
+    player2.y = encoder_value * Player2.speed
+    if player2.top <= 0: player2.top = 0
+    if player2.bottom >= player2: player2.bottom = screenHeight
+
+
 def printScore(surface):
     global p1score, p2score
     font = pygame.font.Font(None, 72)
     text = font.render(str(p2score), True, gamecolor)
     textRect = text.get_rect()
-    textRect.center = (width/2-30, 42)
+    textRect.center = (screenWidth / 2 - 30, 42)
     surface.blit(text, textRect)
     text = font.render(str(p1score), True, gamecolor)
     textRect = text.get_rect()
-    textRect.center = (width/2+30, 42)
+    textRect.center = (screenWidth / 2 + 30, 42)
     surface.blit(text, textRect)
 
 
@@ -103,17 +100,19 @@ start = pygame.mixer.Sound('start.ogg')
 # Ekranı ayarla
 pygame.display.set_caption("Test")
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-width, height = screen.get_size()
+screenWidth, screenHeight = screen.get_size()
 bgcolor = pygame.Color('grey12')
 gamecolor = pygame.Color('white')
 
-ball = pygame.Rect(width/2-15, height/2-15, 30, 30)
+ball = pygame.Rect(screenWidth / 2 - 15, screenHeight / 2 - 15, 30, 30)
 ballcolor = pygame.Color('white')
 ballspeedx = ballspeedy = 0
 ballRestart()
 
-player1 = pygame.Rect(width - 30, height // 2 - 70, 20, 140)
-player2 = pygame.Rect(10, height // 2 - 70, 20, 140)
+player1 = pygame.Rect(screenWidth - Player1.width / 2,
+                      screenHeight / 2, Player1.width, Player1.height)
+player2 = pygame.Rect(Player2.width / 2, screenHeight /
+                      2, Player2.width, Player2.height)
 
 p1score = 0
 p2score = 0
@@ -142,16 +141,18 @@ while True:
     if enkoder1_clk != enkoder1_clkLastState:
         if enkoder1_dt != enkoder1_clk:
             enkoder1_value += 1
+            enkoder1_clkLastState = enkoder1_clk
         else:
             enkoder1_value -= 1
-    enkoder1_clkLastState = enkoder1_clk
+            enkoder1_clkLastState = enkoder1_clk
 
     if enkoder2_clk != enkoder2_clkLastState:
         if enkoder2_dt != enkoder2_clk:
             enkoder2_value += 1
+            enkoder2_clkLastState = enkoder2_clk
         else:
             enkoder2_value -= 1
-    enkoder2_clkLastState = enkoder2_clk
+            enkoder2_clkLastState = enkoder2_clk
 
     # Oyun mantığını işle
     ballAnimation()
@@ -161,7 +162,8 @@ while True:
     # Ekranı temizle ve çizimleri yap
     screen.fill(bgcolor)
     printScore(screen)
-    pygame.draw.aaline(screen, gamecolor, (width/2, 0), (width/2, height))
+    pygame.draw.aaline(screen, gamecolor, (screenWidth / 2,
+                       0), (screenWidth / 2, screenHeight))
     pygame.draw.rect(screen, gamecolor, player1)
     pygame.draw.rect(screen, gamecolor, player2)
     pygame.draw.ellipse(screen, ballcolor, ball)
