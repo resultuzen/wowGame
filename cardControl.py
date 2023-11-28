570f188ec77fffbd4d7019dbc9ff6a71cd053db4
Resul TÜZEN <resultuzen@gmail.com>
2023-11-28 22:42:02 +0300

    Update cardControl.py

--- cardControl.py
+++ cardControl.py
@@ -1,9 +1,23 @@
 import RPi.GPIO as GPIO
+import time
 
-kartOkuyucuPin = 21
+button_pin = 21
+
+# GPIO ayarları
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(kartOkuyucuPin, GPIO.IN)
+GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-while True:
-    
-    print(GPIO.input(kartOkuyucuPin))
+try:
+    print("Buton okuma başlatıldı. Çıkış yapmak için Ctrl+C'ye basın.")
+
+    while True:
+        button_state = GPIO.input(button_pin)
+
+        if button_state == GPIO.LOW:
+            print("Buton basıldı!")
+
+        time.sleep(0.1)
+
+finally:
+    # GPIO pinlerini temizle
+    GPIO.cleanup()
