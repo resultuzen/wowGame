b10c71f68db12e2d6deff0f827649af571e89f8d
Resul TÜZEN <resultuzen@gmail.com>
2023-12-07 01:39:53 +0300

    Update ledControl.py

--- ledControl.py
+++ ledControl.py
@@ -1,14 +1,7 @@
-import time
-import RPi.GPIO as GPIO
-import os
-import sys
-import random
 import board
 import neopixel
-
-GPIO.setmode(GPIO.BCM)
-
-pixel_pin = board.D18
+import time
+pixels = neopixel.NeoPixel(board.D18, 30)
 
 group1 = 172
 group2 = 272
@@ -20,10 +13,9 @@ steps = group1
 num_pixels = 546
 ORDER = neopixel.GRB
 
-pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.5, auto_write=False, pixel_order=ORDER)
-
 while True:
     for _ in range(steps):
-            pixels.fill(pixels[0], (0, 0 , 255))
+            pixels[0] = (255, 0, 0)
+            pixels.fill((0, 255, 0))
             pixels.show()
             time.sleep(0.05)
