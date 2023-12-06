1553b1ba287cab15b63513835bd054f564081c13
Resul TÜZEN <resultuzen@gmail.com>
2023-12-07 01:48:15 +0300

    Update ledControl.py

--- ledControl.py
+++ ledControl.py
@@ -10,8 +10,8 @@ group1_end = 174
 group2_start = 175
 group2_end = 272
 group3_start = 273
-group3_end = 443
-group4_start = 444
+group3_end = 447
+group4_start = 448
 group4_end = 545
 
 while True:
