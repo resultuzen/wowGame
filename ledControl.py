529780280a0ea1e5d7fa0c1c417e7b32533d4ae3
Resul TÜZEN <resultuzen@gmail.com>
2023-12-07 01:34:52 +0300

    Update ledControl.py

--- ledControl.py
+++ ledControl.py
@@ -9,26 +9,21 @@ import neopixel
 GPIO.setmode(GPIO.BCM)
 
 pixel_pin = board.D18
+
+group1 = 172
+group2 = 272
+group3 = 372
+group4 = 546
+
+steps = group1
+
 num_pixels = 546
 ORDER = neopixel.GRB
 
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.5, auto_write=False, pixel_order=ORDER)
 
-def random_color():
-    return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-
-def smooth_transition(color1, color2, steps):
-    transition = [(int(color1[i] + (color2[i] - color1[i]) / steps)) for i in range(3)]
-    return transition
-
-def dance_effect(duration, steps):
-    for _ in range(int(duration / steps)):
-        target_color = random_color()
-
-        for _ in range(steps):
-            pixels.fill(smooth_transition(pixels[0], target_color, steps))
+while True:
+    for _ in range(steps):
+            pixels.fill(pixels[0], (0, 0 , 255), steps)
             pixels.show()
             time.sleep(0.05)
-
-while True:
-    dance_effect(60, 1)  # 60 saniye boyunca, her 5 adımda bir renk geçiş
