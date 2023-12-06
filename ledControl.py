06cfea1a99c46d4b6af5d283e36246b068a2756e
Resul TÜZEN <resultuzen@gmail.com>
2023-12-07 01:29:00 +0300

    Update ledControl.py

--- ledControl.py
+++ ledControl.py
@@ -9,7 +9,7 @@ import neopixel
 GPIO.setmode(GPIO.BCM)
 
 pixel_pin = board.D18
-num_pixels = 542
+num_pixels = 546
 ORDER = neopixel.GRB
 
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.5, auto_write=False, pixel_order=ORDER)
