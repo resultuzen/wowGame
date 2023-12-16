24230099d7886ce14515bb26852f3760df7a28c2
Resul TÜZEN <resultuzen@gmail.com>
2023-12-17 01:48:22 +0300

    Ekran sınır denemesi!

--- wowGame.py
+++ wowGame.py
@@ -202,8 +202,26 @@ while True:
                 pygame.quit()
                 sys.exit()
 
-        sagEnkoderDegeri = sagEncoder.getValue()
-        solEnkoderDegeri = solEncoder.getValue()
+        if not(sagOyuncu.top <= 0) or not(sagOyuncu.bottom >= height):
+            sagEnkoderDegeri = sagEncoder.getValue()
+
+        if sagOyuncu.top <= 0:
+            sagEnkoderDegeri = -25 #Sağ enkoderin minumum değeri öğrenilecek, hatta hesaplama yaptırılırsa daha iyi olur!
+
+        if sagOyuncu.bottom >= height:
+            sagEnkoderDegeri = 25 #Sağ enkoderin maksimum değeri öğrenilecek, hatta hesaplama yaptırılırsa daha iyi olur!
+
+        if not(solOyuncu.top <= 0) or not(solOyuncu.bottom >= height):
+            solEnkoderDegeri = solEncoder.getValue()
+
+        if solOyuncu.top <= 0:
+            solEnkoderDegeri = -25 #Sol enkoderin minumum değeri öğrenilecek, hatta hesaplama yaptırılırsa daha iyi olur!
+
+        if solOyuncu.bottom >= height:
+            solEnkoderDegeri = 25 #Sol enkoderin maksimum değeri öğrenilecek, hatta hesaplama yaptırılırsa daha iyi olur!
+
+        #sagEnkoderDegeri = sagEncoder.getValue()
+        #solEnkoderDegeri = solEncoder.getValue()
     
         # Oyun mantığını işle
         ballAnimation()
