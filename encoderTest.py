ff623a55f718e31ae0b700f523513c534b1871b8
Resul TÜZEN <resultuzen@gmail.com>
2023-12-17 01:58:06 +0300

    Update encoderTest.py

--- encoderTest.py
+++ encoderTest.py
@@ -22,28 +22,7 @@ sagEncoder = Encoder(sagEnkoderDataPin, sagEnkoderClockPin)
 solEnkoderDegeri = 0
 sagEnkoderDegeri = 0
 
-while True:    
+while True: 
     
-    if not(sagOyuncu.top <= 0) or not(sagOyuncu.bottom >= height):
-        sagEnkoderDegeri = sagEncoder.getValue()
-
-    if sagOyuncu.top <= 0:
-        sagEnkoderDegeri = -25 #Sağ enkoderin minumum değeri öğrenilecek, hatta hesaplama yaptırılırsa daha iyi olur!
-
-    if sagOyuncu.bottom >= height:
-        sagEnkoderDegeri = 25 #Sağ enkoderin maksimum değeri öğrenilecek, hatta hesaplama yaptırılırsa daha iyi olur!
-
-    if not(solOyuncu.top <= 0) or not(solOyuncu.bottom >= height):
-        solEnkoderDegeri = solEncoder.getValue()
-
-    if solOyuncu.top <= 0:
-        solEnkoderDegeri = -25 #Sol enkoderin minumum değeri öğrenilecek, hatta hesaplama yaptırılırsa daha iyi olur!
-
-    if solOyuncu.bottom >= height:
-        solEnkoderDegeri = 25 #Sol enkoderin maksimum değeri öğrenilecek, hatta hesaplama yaptırılırsa daha iyi olur!
-
-    #sagEnkoderDegeri = sagEncoder.getValue()
-    #solEnkoderDegeri = solEncoder.getValue()
-
-    print("Sağ:",sagEnkoderDegeri)
-    print("Sol:",solEnkoderDegeri)
+    print("Sağ:",sagEncoder.getValue())
+    print("Sol:",solEncoder.getValue())
