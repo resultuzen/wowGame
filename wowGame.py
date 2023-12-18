3e96807efd1cdeb50384ab0e1b35264a6d8a300e
Resul TÜZEN <resultuzen@gmail.com>
2023-12-19 00:56:11 +0300

    Update wowGame.py

--- wowGame.py
+++ wowGame.py
@@ -222,8 +222,6 @@ while True:
         print("Sag oyuncu bottom:", sagOyuncu.bottom, " Sag oyuncu top: ", sagOyuncu.top)
         print("Sol oyuncu bottom:", solOyuncu.bottom, " Sol oyuncu top:", solOyuncu.top)
 
-        time.sleep(0.5)
-
         # Oyun mantığını işle
         ballAnimation()
         sagOyuncuAnimation(sagEnkoderDegeri)
