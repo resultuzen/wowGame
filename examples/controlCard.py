3a0ea1156d37885afc1a3f6cc46317ddabc29700
Resul TÜZEN <resultuzen@gmail.com>
2024-02-22 21:54:15 +0300

    Update controlCard.py

--- examples/controlCard.py
+++ examples/controlCard.py
@@ -1,11 +1,27 @@
-import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+import RPi.GPIO as GPIO
 
-def button_callback(channel):
-    print("Button was pushed!")
-  
-GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-GPIO.add_event_detect(21,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
-message = input("Press enter to quit\n\n") # Run until someone presses enter
-GPIO.cleanup() # Clean up
+# Kullanmak istediğiniz GPIO pin numarasını buraya girin
+buton_pin = 21
+
+# GPIO ayarları
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(buton_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+try:
+    print("Buton okuma başladı. Çıkış yapmak için Ctrl+C'ye basın.")
+    while True:
+        # Buton durumunu oku
+        buton_durumu = GPIO.input(buton_pin)
+
+        # Buton basıldığında durumu 0, basılmadığında durumu 1 olacaktır
+        if buton_durumu == 0:
+            print("Buton basıldı!")
+        else:
+            print("Buton bırakıldı!")
+
+except KeyboardInterrupt:
+    print("Program kapatılıyor.")
+
+finally:
+    # GPIO pinlerini temizle
+    GPIO.cleanup()
