958df48f3b562cfeddf1369b9ccedf72b2aef24d
Resul TÜZEN <resultuzen@gmail.com>
2024-02-26 22:26:31 +0300

    Update controlCard.py

--- examples/controlCard.py
+++ examples/controlCard.py
@@ -1,27 +1,30 @@
 import RPi.GPIO as GPIO
+import time
 
-# Kullanmak istediğiniz GPIO pin numarasını buraya girin
-buton_pin = 21
+# Pin numarasını belirtin
+buton_pin = 17
 
-# GPIO ayarları
+# GPIO modunu belirleme
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(buton_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# Pin'i giriş olarak ayarlama ve pull-down direnci etkinleştirme
+GPIO.setup(buton_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+# Interrupt tetikleyiciyi ayarlama fonksiyonu
+def button_callback(channel):
+    print("Button pressed on pin {}".format(channel))
+
+# GPIO üzerinde interrupt tetikleyiciyi tanımlama
+GPIO.add_event_detect(buton_pin, GPIO.RISING, callback=button_callback, bouncetime=300)
 
 try:
-    print("Buton okuma başladı. Çıkış yapmak için Ctrl+C'ye basın.")
+    print("Press Ctrl+C to exit")
     while True:
-        # Buton durumunu oku
-        buton_durumu = GPIO.input(buton_pin)
-
-        # Buton basıldığında durumu 0, basılmadığında durumu 1 olacaktır
-        if buton_durumu == 0:
-            print("Buton basıldı!")
-        else:
-            print("Buton bırakıldı!")
+        time.sleep(1)
 
 except KeyboardInterrupt:
-    print("Program kapatılıyor.")
+    pass
 
 finally:
-    # GPIO pinlerini temizle
+    # GPIO temizleme
     GPIO.cleanup()
