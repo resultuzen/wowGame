8ee8ddd71f71fbbc9a70a1740f456c8f2aeab1c3
Resul TÜZEN <resultuzen@gmail.com>
2024-02-27 00:49:52 +0300

    Update vlc.py

--- examples/vlc.py
+++ examples/vlc.py
@@ -5,7 +5,7 @@ pygame.init()
 video = moviepy.editor.VideoFileClip("../video/acilisVideo.mp4")
 
 video = video.volumex(0.5)
-#video.ipython_display(width = 1920, height = 1080)
+video.resize((1920, 1080))
 
-video.preview(width = 1920, height = 1080)
+video.preview()
 pygame.quit()
