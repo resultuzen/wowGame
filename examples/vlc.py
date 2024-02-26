4e81c35f031be7c8ed3b1ebd38f0c08d03eae949
Resul TÜZEN <resultuzen@gmail.com>
2024-02-27 00:07:59 +0300

    Create vlc.py

---
+++ examples/vlc.py
@@ -0,0 +1,25 @@
+import vlc
+import time
+
+# VLC Media Player'ın başlatılması
+instance = vlc.Instance()
+player = instance.media_player_new()
+
+# Oynatılacak video dosyasının belirtilmesi
+media = instance.media_new("../video/acilisVideo.mp4")
+player.set_media(media)
+
+# Pencere oluşturma
+win = vlc.MediaPlayer()
+
+# Video penceresini oluşturma
+win.set_media(media)
+
+# Pencereyi görünür hale getirme
+win.set_hwnd(int(video_frame_id))
+
+# Videoyu oynatma
+player.play()
+
+# Videoyu durdurma
+player.stop()
