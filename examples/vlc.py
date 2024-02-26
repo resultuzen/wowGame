1e12c12fb2ba449c5dc645d3996e31a7bec88009
Resul TÜZEN <resultuzen@gmail.com>
2024-02-27 00:40:00 +0300

    Update vlc.py

--- examples/vlc.py
+++ examples/vlc.py
@@ -1,7 +1,11 @@
 import pygame
-import moviepy.editor
+import moviepy.editor import *
 
 pygame.init()
-video = moviepy.editor.VideoFileClip("../video/acilisVideo.mp4")
-video.preview()
+video = VideoFileClip("../video/acilisVideo.mp4")
+#video.preview()
+
+video = video.volumex(0.5)
+video.ipython_display(width = 1920, height = 1080)
+
 pygame.quit()
