aa4cd9bf16b1de9e96b56d2509146b979187f8b3
Resul TÜZEN <resultuzen@gmail.com>
2024-02-27 00:58:15 +0300

    Update vlc.py

--- examples/vlc.py
+++ examples/vlc.py
@@ -4,5 +4,14 @@ from moviepy.editor import VideoFileClip
 video_path = "../video/acilisVideo.mp4"
 video_clip = VideoFileClip(video_path)
 
+original_width, original_height = video_clip.size
+
+# Yeni genişlik ve yüksekliği belirleyin
+new_width = 1920  # örnek olarak 1920 piksel
+new_height = 1080  # örnek olarak 1080 piksel
+
 # Videoyu gösterin (örneğin, bir pencerede oynatmak için)
-video_clip.preview()
+resized_video_clip = video_clip.resize(width=new_width, height=new_height)
+
+# Videoyu gösterin (örneğin, bir pencerede oynatmak için)
+resized_video_clip.preview()
