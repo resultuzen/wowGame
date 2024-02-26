721c94e7f5ce2053895d71c888fb5e7686beb83f
Resul TÜZEN <resultuzen@gmail.com>
2024-02-27 00:16:41 +0300

    Update vlc.py

--- examples/vlc.py
+++ examples/vlc.py
@@ -1,24 +1,17 @@
+# importing vlc module
 import vlc
+ 
+# importing time module
 import time
-
-# VLC Media Player'ın başlatılması
-instance = vlc.Instance("--no-xlib")  # veya instance = vlc.Instance() (gerekli duruma bağlı olarak)
-player = instance.media_player_new()
-
-# Oynatılacak video dosyasının belirtilmesi
+ 
+# creating vlc media player object
+media_player = vlc.MediaPlayer()
+ 
+# media object
 media = instance.media_new("../video/acilisVideo.mp4")
-player.set_media(media)
-
-# Pencere oluşturma
-win = vlc.MediaPlayer()
-
-# Video penceresini oluşturma
-win.set_media(media)
-
-# Pencereyi görünür hale getirme
-win.set_hwnd(int(video_frame_id))
-
-# Videoyu oynatma
-player.play()
-
-player.stop()
+ 
+# setting media to the media player
+media_player.set_media(media)
+ 
+# start playing video
+media_player.play()
