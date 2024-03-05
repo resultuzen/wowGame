5c37584ea71163144fe0da05055d56096f7435d6
Resul TÜZEN <resultuzen@gmail.com>
2024-03-05 22:23:21 +0300

    Update controlCard.py

--- examples/controlCard.py
+++ examples/controlCard.py
@@ -10,21 +10,6 @@ GPIO.setmode(GPIO.BCM)
 # Pin'i giriş olarak ayarlama ve pull-down direnci etkinleştirme
 GPIO.setup(buton_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-# Interrupt tetikleyiciyi ayarlama fonksiyonu
-def button_callback(channel):
-    print("Button pressed on pin {}".format(channel))
-
-# GPIO üzerinde interrupt tetikleyiciyi tanımlama
-GPIO.add_event_detect(buton_pin, GPIO.RISING, callback=button_callback, bouncetime=300)
-
-try:
-    print("Press Ctrl+C to exit")
-    while True:
-        time.sleep(1)
-
-except KeyboardInterrupt:
-    pass
-
-finally:
-    # GPIO temizleme
-    GPIO.cleanup()
+while True:
+    if GPIO.input(buton_pin) == GPIO.HIGH:
+        print("Hey")
